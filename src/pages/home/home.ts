48a68d8223358dff3a4272dec69e61e7a507411f
Francisco Bardin <franciscobardin@gmail.com>
2017-11-24 08:12:02 -0300

    Anda

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,65 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams, ViewController, ModalController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  arrayTodos = [];
+  myForm: FormGroup;
+  objeto = {
+    name: String,
+    price: Number,
   }
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl : ViewController,
+    public formBuilder: FormBuilder, private modal: ModalController, public storage: Storage) {
+      this.getData()
+      // storage.clear();
+      this.myForm = this.formBuilder.group({
+        name: ['', Validators.required],
+        price: ['', Validators.required]
+      });
+  }
+
+  openModal() {
+    const myModal = this.modal.create('ModalPage');
+    myModal.present();
+  }
+
+   getData() {
+    this.storage.keys().then((arrayDeKeys) => {
+      console.log(arrayDeKeys);
+      this.arrayTodos = [];
+      for(let i of arrayDeKeys){
+        this.storage.get(i).then((pepe) => {
+          let obj = JSON.parse(pepe);
+          this.arrayTodos.push(obj);
+          console.log(obj);
+        });
+      }
+    });
+  }
+
+async saveData() {
+  this.objeto.name = this.myForm.get('name').value;
+  this.objeto.price = this.myForm.get('price').value;
+
+  //Crea una key para el objeto que se esta guardando
+  const num = await this.storage.length()
+    let messi = num +1 ;
+    let key = "" + messi;
+    console.log(key);
+  //Guardo el objeto en storage
+    let stringfyObj = JSON.stringify(this.objeto, null);
+    this.storage.set(key , stringfyObj);
+  console.log(this.objeto);
+  this.navCtrl.setRoot(HomePage);
+  this.navCtrl.popToRoot();
+  //this.getData();
+}
 
 }
---
+++ src/pages/modal/modal.ts
@@ -0,0 +1,87 @@
+import { Component } from '@angular/core';
+import { IonicPage, ViewController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+
+/**
+ * Generated class for the ModalPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-modal',
+  templateUrl: 'modal.html',
+})
+export class ModalPage {
+  people = [];
+  debtor = [];
+  done = [];
+  benefited = [];
+  end = [];
+
+  constructor(private view: ViewController, public storage: Storage) {
+    this.getData()
+  }
+
+  closeModal() {
+    this.view.dismiss();
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ModalPage');
+  }
+
+  async getData() {
+    let total = 0;
+    const arrayDeKeys = await this.storage.keys()
+    console.log(arrayDeKeys);
+    this.people = [];
+    for (let i of arrayDeKeys) {
+      const pepe = await this.storage.get(i);
+      let obj = JSON.parse(pepe);
+      this.people.push(obj);
+    }
+    for (let i of Object.keys(this.people)) {
+      total += parseInt(this.people[i].price);
+    }
+    let quotient = total / this.people.length
+    for (let i of Object.keys(this.people)) {
+      let price = (Math.abs(parseInt(this.people[i].price)))
+      if (price < quotient) {
+        this.people[i].price = Math.abs(price - quotient)
+        this.people[i].state = "pagan";
+        this.debtor.push(this.people[i]);
+
+      }else if (price == quotient){
+        this.people[i].price = price - quotient
+        this.people[i].state = "nada"
+        this.done.push(this.people[i]);
+
+      }
+      else {
+        this.people[i].price = price - quotient
+        this.people[i].state = "beneficiado"
+        this.benefited.push(this.people[i]);
+      }
+    }
+      for (let i of Object.keys(this.debtor)){
+        for (let j of Object.keys(this.benefited)){
+                    console.log(this.debtor[i].name + this.debtor[i].price + "+" + this.benefited[j].price)
+          if (parseInt(this.debtor[i].price) <= parseInt(this.benefited[j].price)){
+            this.end.push({text:`${this.debtor[i].name} paga a ${this.benefited[j].name} ${this.debtor[i].price}`})
+            this.benefited[j].price =  this.benefited[j].price - this.debtor[i].price
+            this.debtor[i].price = 0
+          }else{
+            this.end.push({text:`${this.debtor[i].name} paga A ${this.benefited[j].name} ${this.benefited[j].price}`})
+            //this.benefited[j].price = this.debtor[i].price - this.benefited[j].price
+            this.debtor[i].price = this.debtor[i].price - this.benefited[j].price
+            this.benefited[j].price = 0
+
+          }
+        }
+      }
+  }
+
+}
